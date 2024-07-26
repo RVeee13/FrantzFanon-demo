769d57c39f368f334a78b58366709fff90df0eca
RVeee13 <you@example.com>
2024-07-26 20:57:47 +0100

    the loader problem fixed

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -24,9 +24,23 @@ import Loader from '../components/Loader'
 export const Home = () => {
     const Images = [img1, img2, img3, img4]
     const [loading, setLoading] = useState(true);
-    console.log(loading)
 
-    window.addEventListener('load', ()=>{setLoading(false)})
+    useEffect(() => {
+    // callback function to call when event triggers
+    const onPageLoad = () => {
+      console.log('page loaded');
+      setLoading(false)
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === 'complete') {
+      onPageLoad()
+    } else {
+      window.addEventListener('load', onPageLoad, false);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+  }, []);
 
   return (
 
